40fad9897153b92dfed0614fe2d0a1952716d21f
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2022-09-12 10:32:15 -0300

    primeiro commit

---
+++ src/resultadoEscolar.java
@@ -0,0 +1,22 @@
+public class resultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        /* int nota = 10;
+        
+        if(nota >= 7)
+
+        System.out.println("Aprovado");
+
+        else if ( nota >= 5 && nota < 7)
+        System.out.println("Prova de Recuperação");
+
+        else */
+          //int nota = 7;
+       // String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+
+    }
+}
